76992f48b256fc77bbc1895a3a9df499508d568f
Nguyễn Văn Công <congnguyenlop9a1@gmail.com>
2020-02-28 22:59:12 +0700

    Add index.html 403 for some directory folder

--- application/views/index.php
+++ application/views/index.php
@@ -85,9 +85,6 @@
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a
-				href="user_guide/">User Guide</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
